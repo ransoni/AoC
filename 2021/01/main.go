fa014d2bb81748d78826bb7f9aa381baf1666508
Mika Tuominen <mika@ridefree.fi>
2021-12-01 11:31:27 +0200

    01/02

--- 2021/01/main.go
+++ 2021/01/main.go
@@ -2,29 +2,68 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/ransoni/AoC/helpers"
 )
 
 func main() {
-	data := helpers.ReadFileAsArray("data.txt")
+	// data := helpers.ReadFileAsArray("data.txt")
+	data := helpers.ReadFileAsIntArray("data.txt")
 
+	fmt.Printf("\nOne by One Results:\n")
 	increments(data)
+
+	fmt.Printf("\nSliding Window Results:\n")
+	slidingWindow(data)
 }
 
-func increments(data []string) {
+func slidingWindow(data []int) {
+	var inc int
+	var dec int
+
+	for i := range data {
+		// fmt.Printf("Len of window: %d", len(data[i:]))
+		if len(data[i:]) < 3 {
+			// fmt.Printf("Not enough rows available!\n")
+			continue
+		}
+
+		prevWindow := countWindow(data[i : i+3])
+		nextWindow := countWindow(data[i+1 : i+4])
+		if nextWindow > prevWindow {
+			// fmt.Printf("Its BIGGER!\n")
+			// fmt.Printf("Prev: %v || %v :Next\n", prevWindow, nextWindow)
+			inc++
+		} else {
+			// fmt.Printf("Its smaller!\n")
+			// fmt.Printf("Prev: %v || %v :Next\n", prevWindow, nextWindow)
+			dec++
+		}
+	}
+
+	fmt.Printf("Increased: %v\n", inc)
+	fmt.Printf("Decreased: %v\n", dec)
+
+	// return
+}
+
+func countWindow(window []int) int {
+	var sum int
+
+	for _, num := range window {
+		sum = sum + num
+	}
+
+	return sum
+}
+
+func increments(data []int) {
 
 	var inc int
 	var dec int
 	var prev int
 
-	for i, k := range data {
-		num, err := strconv.Atoi(k)
-		if err != nil {
-			fmt.Printf("Could not convert!")
-		}
-
+	for i, num := range data {
 		if i == 0 {
 			prev = num
 			continue
